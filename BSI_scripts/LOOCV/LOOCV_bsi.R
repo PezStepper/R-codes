fb22682841ab3f8b37b9abc3e06ba67df81882b8
PezStepper <pezstepper@gmail.com>
2017-06-27 15:40:57 +1200

    Add files via upload

---
+++ BSI_scripts/LOOCV/LOOCV_bsi.R
@@ -0,0 +1,126 @@
+#############################################################################################################
+## This script takes the score vectors for each site and combines them in to a matrix to be correlated     ##
+## with bed movement data. One site is manually left out each time for LOOCV.                              ##
+##                                                                                                         ##
+## At Import individual site score vectors use a # to leave a site out                                     ##
+##                                                                                                         ##
+## At bed.movement.T need to leave one row out from bed.movement depending on which site you are leaving   ##
+## out                                                                                                     ##
+#############################################################################################################
+
+# Sites
+# Manawatu
+# Ngamoko
+# Coppermine
+# Mangapuaka
+# Tamaki
+# Mangatautou
+# Matanganui
+# Matanganui Tributary
+# Limestone Creek
+# Coal Creek
+
+
+#### Import individual site score vectors ####
+
+setwd("F:/Andrew BSI DATA/Individual Site Scores")
+
+Manawatu <- read.delim(file="Manawatu_scores.txt", header = TRUE)
+Ngamoko <- read.delim(file="Ngamoko_scores.txt", header = TRUE)
+Coppermine <- read.delim(file="Coppermine_scores.txt", header = TRUE)
+Mangapuaka <- read.delim(file="Mangapuaka_scores.txt", header = TRUE)
+Tamaki <- read.delim(file="Tamaki_scores.txt", header = TRUE)
+Mangatautou <- read.delim(file="Mangatautou_scores.txt", header = TRUE)
+Matanganui <- read.delim(file="Matanganui_scores.txt", header = TRUE)
+Matanganui.Tributary <- read.delim(file="Matanganui_Tributary_scores.txt", header = TRUE)
+Limestone <- read.delim(file="Limestone_scores.txt", header = TRUE)
+Coal <- read.delim(file="Coal_scores.txt", header = TRUE)
+
+bed.movement <- read.delim(file= "H:/PhD Data/BSI/2013-14 Summer Scholarship/PhD/bed_movement.txt", header = TRUE, row.names = 1)
+
+#### LOOCV (Leave One Out Cross Validation) ####
+
+### Merge vectors to a matrix
+
+# REMEMBER TO LEAVE ONE VALIDATION SITE OUT
+
+setwd("F:/Andrew BSI DATA/LOOCV/Coal Left Out") # change this to the folder you want the output dataframes in
+
+# Create a matrix of all the possible scores for each site
+
+score.matrix <- data.frame(Manawatu
+                          , Ngamoko
+                          , Coppermine
+                          , Mangapuaka
+                          , Tamaki
+                          , Mangatautou
+                          , Matanganui
+                          , Matanganui.Tributary
+                          , Limestone
+                          #, Coal
+                          )
+
+head(score.matrix)
+
+#### Export Data ####
+
+write.table(score.matrix, file="score_matrix.txt", sep="\t") #assumes we are saving tot he working directory
+
+### Code for correlation between bed movement and scores
+
+#### Transpose the bed movement to have sites as columns ####
+
+#install.packages("optimbase")
+#library(optimbase)
+
+as.data.frame(bed.movement) #convert data.frame to matrix
+
+bed.movement.T <- t(bed.movement[-10,]) #transpose the data, and drop column i to leave one out
+
+View(bed.movement.T)
+
+
+#### Correlation ####
+
+#https://stat.ethz.ch/pipermail/r-help//2009-November/409838.html
+
+# Needed to concantenate the data as it was reading in as a list rather than numeric data.
+
+concant.matrix <- data.frame(matrix(ncol = 9, nrow = length(score.matrix[,1]))) #creates emptp matrix with 9 columns for 9 sites 
+#and rows equal to total possible score combinations
+
+# Now we will loop through the score matrix and concantenate each row and put it in the concant matrix. Note this can be modified for parallel processing.
+
+for(i in 1:length(score.matrix[,1])){ #for each row
+  for(j in 1:9){ #for each site
+    concant.matrix[i,j] <- c(score.matrix[i,j])
+    }
+  } 
+
+head(concant.matrix) #shows the first few rows of data. Use to manually validate process worked
+
+tail(concant.matrix) # Shows the last few rows
+
+write.table(concant.matrix, file="all_sites_scores_matrix.txt", sep="\t") #output thes the matrix
+
+correlation.matrix <- data.frame(matrix(ncol = 1, nrow = length(score.matrix[,1]))) #creates an empty dataframe to store the
+#correlations of each score combination vs bed movement data. This data will be used to find the highest correlation achieved
+#and trace back using the row number to find the combinations of parameter weightings used
+
+bed.concant <- c(bed.movement.T)
+
+# Now we will loop through and do the correlation on each row. Note this can be modified for parallel processing.
+
+for(i in 1:length(score.matrix[,1])){
+  correlation.matrix[i,] <- cor(as.numeric(concant.matrix[i,]), as.vector(bed.concant))
+} 
+
+head(correlation.matrix)
+
+tail(correlation.matrix)
+
+write.table(correlation.matrix, file="correlation_matrix.txt", sep="\t")
+
+max(correlation.matrix) # Find the max correlation in the 1 row
+
+which.max(correlation.matrix[,1]) # Find the column number which contains the max correlation
